b9031ea8ef1b02fe4cf868edbb79b0ee0ac82ff0
ethanburkett <ethan@ethanburkett.net>
2023-10-31 12:00:00 -0500

    Updated CLI

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import inq, { QuestionCollection } from "inquirer";
-import fs from "node:fs";
-import { spawn } from "bun";
-import chalk from "chalk";
+const fs = require("fs");
+import { exec as spawn } from "node:child_process";
+const colors = require("colors");
 
 const command = process.argv[2];
 
@@ -36,60 +36,61 @@ switch (command) {
     break;
 }
 
-const answers = await inq.prompt(questions);
+(async () => {
+  const answers = await inq.prompt(questions);
 
-if (answers.type) {
-  if (["Command", "Event"].includes(answers.type)) {
-    console.log(
-      `${process.cwd()}/src/${answers.type.toLowerCase()}s/${answers.name}.ts`
-    );
+  if (answers.type) {
+    if (["Command", "Event"].includes(answers.type)) {
+      const template = fs
+        .readFileSync(
+          `${__dirname}/template/${answers.type.toLowerCase()}.ts`,
+          "utf-8"
+        )
+        .replace(/{cmd_name}/g, answers.name)
+        .replace(/{event_name}/g, answers.name);
 
-    const template = fs
-      .readFileSync(
-        `${__dirname}/template/${answers.type.toLowerCase()}.ts`,
-        "utf-8"
-      )
-      .replace(/{cmd_name}/g, answers.name)
-      .replace(/{event_name}/g, answers.name);
-
-    fs.writeFileSync(
-      `${process.cwd()}/src/${answers.type.toLowerCase()}s/${answers.name}.ts`,
-      template
-    );
-  }
-} else if (answers.path) {
-  try {
-    fs.mkdirSync(answers.path);
-    fs.mkdirSync(`${answers.path}/src`);
-    fs.mkdirSync(`${answers.path}/src/commands`);
-    fs.mkdirSync(`${answers.path}/src/events`);
-    fs.mkdirSync(`${answers.path}/src/middleware`);
-
-    const index = fs.readFileSync(`${__dirname}/template/index.ts`, "utf-8");
-    const command = fs.readFileSync(
-      `${__dirname}/template/command.ts`,
-      "utf-8"
-    );
-
-    fs.writeFileSync(`${answers.path}/index.ts`, `// You can delete me :)`);
-    fs.writeFileSync(`${answers.path}/src/index.ts`, index);
-    fs.writeFileSync(`${answers.path}/src/commands/ping.ts`, command);
-
-    const { stderr } = spawn(["bun", "init", "-y"], {
-      cwd: answers.path,
-    });
-
-    if (stderr) {
-      console.log(stderr);
+      fs.writeFileSync(
+        `${process.cwd()}/src/${answers.type.toLowerCase()}s/${
+          answers.name
+        }.ts`,
+        template
+      );
     }
+  } else if (answers.path) {
+    try {
+      if (!fs.existsSync(answers.path)) {
+        fs.mkdirSync(answers.path);
+      }
+      fs.mkdirSync(`${answers.path}/src`);
+      fs.mkdirSync(`${answers.path}/src/commands`);
+      fs.mkdirSync(`${answers.path}/src/events`);
+      fs.mkdirSync(`${answers.path}/src/middleware`);
 
-    spawn(["bun", "add", "disbun"], {
-      cwd: answers.path,
-    });
-  } catch (err) {
-    console.log(
-      chalk.red`!`,
-      chalk.bold`Seems that directory name is already taken..`
-    );
+      const index = fs.readFileSync(`${__dirname}/template/index.ts`, "utf-8");
+      const command = fs.readFileSync(
+        `${__dirname}/template/command.ts`,
+        "utf-8"
+      );
+
+      fs.writeFileSync(`${answers.path}/index.ts`, `// You can delete me :)`);
+      fs.writeFileSync(`${answers.path}/src/index.ts`, index);
+      fs.writeFileSync(`${answers.path}/src/commands/ping.ts`, command);
+
+      const { stderr } = spawn("bun init -y", {
+        cwd: answers.path,
+      });
+
+      if (stderr) {
+      }
+
+      spawn("bun add disbun", {
+        cwd: answers.path,
+      });
+    } catch (err) {
+      console.log(
+        colors.red(`!`),
+        colors.bold(`Seems that directory name is already taken..`)
+      );
+    }
   }
-}
+})();
